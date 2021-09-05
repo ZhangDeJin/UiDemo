03daaa0f4e5c2f8c385e91b6e6f2e716fd03e9e0
dejinzhang <2439762097@qq.com>
2021-09-05 16:12:29 +0800

    systemfuncationlibrary  UiUtils类中增加了 一些方法的注释，以及增加了一个防止连续快速点击的工具。

--- systemfuncationlibrary/src/main/java/com/zdj/systemfuncationlibrary/UiUtils.java
+++ systemfuncationlibrary/src/main/java/com/zdj/systemfuncationlibrary/UiUtils.java
@@ -14,19 +14,55 @@ import android.widget.EditText;
  * </pre>
  */
 public class UiUtils {
+    /**
+     * 单位转换：dp--->px
+     * @param context  上下文环境
+     * @param dp  原始单位
+     * @return  返回转换后的单位
+     */
     public static int dpToPx(Context context, float dp) {
         float scale = context.getResources().getDisplayMetrics().density;
         return (int)(dp * scale + 0.5f);
     }
 
+    /**
+     * 获取屏幕高度
+     * @param context  上下文环境
+     * @return  返回屏幕高度
+     */
     public static int getHeight(Context context) {
         return context.getResources().getDisplayMetrics().heightPixels;
     }
 
+    /**
+     * 设置EditText提示文字的大小
+     * @param hintText  提示文字
+     * @param size  大小
+     * @param editText  editText
+     */
     public static void setEditTextHintTextSize(String hintText, int size, EditText editText) {
         SpannableString spannableString = new SpannableString(hintText);
         AbsoluteSizeSpan absoluteSizeSpan = new AbsoluteSizeSpan(size, true);
         spannableString.setSpan(absoluteSizeSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         editText.setHint(new SpannableString(spannableString));
+    }
+
+    //两次点击间隔不能少于1000ms
+    private static final int FAST_CLICK_DELAY_TIME = 1000;
+    //上次点击的时间
+    private static long lastClickTime;
+
+    /**
+     * 判断是否为快速点击
+     * @return  返回是否为快速点击
+     */
+    public static boolean isFastClick() {
+        boolean flag = true;
+        long currentClickTime = System.currentTimeMillis();
+        if ((currentClickTime - lastClickTime) >= FAST_CLICK_DELAY_TIME) {
+            flag = false;
+        }
+        lastClickTime = currentClickTime;
+        return flag;
     }
 }
