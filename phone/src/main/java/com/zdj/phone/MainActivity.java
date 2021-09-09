925f7da827a618738b940e177800b79b8a79cc1a
dejinzhang <2439762097@qq.com>
2021-09-09 14:54:19 +0800

    项目中新增一个application module--->Phone

---
+++ phone/src/main/java/com/zdj/phone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zdj.phone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
